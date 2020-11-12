13d540da7047d3bf8f0c51fe991d0174a74fb2df
webashrafians <webashrafians@gmail.com>
2020-11-12 16:40:58 +0600

    wordpress theme aquila

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main emplae file.
+ * 
+ * @package Aquila
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress theme</title>
+</head>
+<body>
+    hello wordpess
+</body>
+</html>
