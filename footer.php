56f3fc7659fcf6c8be816148211b5bbd873a71fe
webashrafians <webashrafians@gmail.com>
2020-11-12 21:42:22 +0600

    add some

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Footer template
+ * 
+ * @package Aquila
+ */
+ ?>
+
+<footer>footer</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,16 +4,12 @@
  * 
  * @package Aquila
  */
+
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress theme</title>
-</head>
-<body>
-    hello wordpess
-</body>
-</html>
+<h3>Hello world</h3>
+
+
+<?php
+get_footer();
